4cb55fb2a907c79c5a3394230377e39dc36ebf97
Andy Snagovsky <dv5ife@gmail.com>
2011-03-08 23:21:57 +0200

    [#10616323] Add classes for keyboard and mouse

---
+++ js/classes.js
@@ -0,0 +1,21 @@
+/**
+ * Ugly global variable (IE style) to make Keyboard.isPressed() work with only one argument.
+ * Other possible explanation of this is that I am a junkie.
+ */
+var e;
+
+function Keyboard(){
+    this.key = {
+        37: 'arrowLeft',
+        39: 'arrowRight'
+    }
+    /*
+     this.lastKey = function(){
+     return e.which
+     }
+     */
+    this.isPressed = function(keyName){
+        return (e.type === 'keydown' && e.which === keyName) ? true : false;
+    }
+	this.set
+}
